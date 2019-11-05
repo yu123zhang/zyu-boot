9fb059ad4b964bcf44adf6edded13b9564ce090d
zyu <18800207862@163.com>
2019-11-05 21:23:27 +0800

    V0.9

--- src/main/java/zyu/gittest/GitTest.java
+++ src/main/java/zyu/gittest/GitTest.java
@@ -2,6 +2,6 @@ package zyu.gittest;
 
 public class GitTest {
     public GitTest(){
-        System.out.println("--branch master V0.5 -- merge合并分支 --reking");
+        System.out.println("--branch master V0.8 -- merge合并分支 --合并reking分支（rebase）");
     }
 }
