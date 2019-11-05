23eeae55c9e34e56a998a8fb85278ebf4704e2f0
zyu <18800207862@163.com>
2019-11-05 21:18:26 +0800

    V0.6

--- src/main/java/zyu/gittest/GitTest.java
+++ src/main/java/zyu/gittest/GitTest.java
@@ -2,6 +2,6 @@ package zyu.gittest;
 
 public class GitTest {
     public GitTest(){
-        System.out.println("--branch master V0.5 -- merge合并分支");
+        System.out.println("--branch master V0.5 -- merge合并分支 --reking");
     }
 }
