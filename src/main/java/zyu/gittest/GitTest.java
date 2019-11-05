df4138037b0cdd105f167f83f3dbce7162940f8e
zyu <18800207862@163.com>
2019-11-05 22:01:23 +0800

    ssh-push

--- src/main/java/zyu/gittest/GitTest.java
+++ src/main/java/zyu/gittest/GitTest.java
@@ -2,6 +2,6 @@ package zyu.gittest;
 
 public class GitTest {
     public GitTest(){
-        System.out.println("--branch master V0.8 -- merge合并分支 --合并reking分支（rebase）");
+        System.out.println("--branch master V1.0 -- merge合并分支 --合并reking分支（rebase）--ssh");
     }
 }
